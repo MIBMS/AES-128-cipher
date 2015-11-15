c7ee6dbcd784d0e69af3cfec4dcec43f2f08ca78
Ilham Salleh <ilhamsalleh91@gmail.com>
2015-11-15 18:29:39 +0800

    ported to python 3

--- AES-128-cipher.py
+++ AES-128-cipher.py
@@ -1,37 +1,40 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 
 '''
 AES-128-cipher.py: Encrypts and decrypts files using AES-128 in either CBC or CTR mode
 '''
 
+import binascii
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
 from Crypto import Random
 
 
+
 def endefun():
 
-    print "\n","You can encrypt plaintexts which will be written to the ciphertextsandkeys\
-.txt file, or decrypt ciphertexts in that file.\n"
+    print()
+    print("You can encrypt plaintexts which will be written to the ciphertextsandkeys\
+.txt file, or decrypt ciphertexts in that file.\n")
     
-    ende = raw_input("Do you want to encrypt plaintexts or decrypt ciphertexts?\
+    ende = input("Do you want to encrypt plaintexts or decrypt ciphertexts?\
     Enter 'e' to encrypt, 'd' to decrypt, 'esc' to quit:\n")
 
 
     if ende == "e":
-        ciptext = raw_input("Plaintext:\n")
+        ciptext = input("Plaintext:\n")
         def getkey():
-            key = raw_input("Please enter a 16 (ASCII) character key:\n")
+            key = input("Please enter a 16 character key:\n")
             if len(key) != 16:
-                print "You have to enter 16 ASCII characters."
+                print("You have to enter 16 characters.")
                 key = getkey()
             return key
         key = getkey()
         def getmode():
-            mode = raw_input("What block cipher mode do you want to use? \
+            mode = input("What block cipher mode do you want to use? \
 Please enter CBC or CTR:\n")
             if mode.upper() != "CBC" and mode.upper() != "CTR":
-                print "You may only enter CBC or CTR."
+                print("You may only enter CBC or CTR.")
                 mode = getmode()
             return mode
         mode = getmode()
@@ -43,24 +46,27 @@ Please enter CBC or CTR:\n")
             encryptor = AES.new(key, AES.MODE_CBC, iv)
             def padding(ciptext):
                 remain = 16-len(ciptext)%16
-                ciptext = bytearray(ciptext, "ascii") + bytearray([remain]*remain)
+                ciptext = bytes(ciptext, 'utf-8') + bytes([remain]*remain)
                 return ciptext
-            ciptext = str(padding(ciptext))
-            val = iv.encode("hex")
-            ciphertext = encryptor.encrypt(ciptext).encode("hex")
-            outputkey = key.encode("hex")
+            ciptext = padding(ciptext)
+            val = binascii.hexlify(iv)
+            ciphertext = binascii.hexlify(encryptor.encrypt(ciptext))
+            outputkey = binascii.hexlify(bytes(key, 'utf-8'))
             with open("ciphertextsandkeys.txt","a") as f:
-                f.write("\n" + "CBC: " + outputkey + ", " + val+ciphertext)
+                f.write("\n" + "CBC: " + outputkey.decode() + ", " + val.decode() \
+                        + ciphertext.decode())
         else:
-            counter = Counter.new(128, initial_value=long(iv.encode("hex"),16))
+            counter = Counter.new(128, initial_value=int.from_bytes(binascii.hexlify(iv), \
+                                                                    byteorder='big'))
             encryptor = AES.new(key, AES.MODE_CTR, counter = counter)
-            val = iv.encode("hex")
-            ciphertext = encryptor.encrypt(ciptext).encode("hex")
-            outputkey = key.encode("hex")
+            val = binascii.hexlify(iv)
+            ciphertext = binascii.hexlify(encryptor.encrypt(ciptext))
+            outputkey = binascii.hexlify(bytes(key,'utf-8'))
             with open("ciphertextsandkeys.txt","a") as f:
-                f.write("\n" + "CTR: " + outputkey + ", " + val +ciphertext)
+                f.write("\n" + "CTR: " + outputkey.decode() + ", " \
+                        + val.decode() +ciphertext.decode())
             f.close()
-        print "Your key and ciphertext have been added to ciphertextsandkeys.txt."
+        print("Your key and ciphertext have been added to ciphertextsandkeys.txt.")
 
         endefun()
         
@@ -97,38 +103,40 @@ Please enter CBC or CTR:\n")
 
 
         def decryptCBC(ciptriple):
-            aes = AES.new(ciptriple[0].decode("hex"), AES.MODE_CBC, ciptriple[2].decode("hex"))
-            plaintext = aes.decrypt(ciptriple[1].decode("hex"))
-            return plaintext
+            aes = AES.new(binascii.unhexlify(bytes(ciptriple[0],'utf-8')), AES.MODE_CBC, \
+                          binascii.unhexlify(bytes(ciptriple[2],'utf-8')))
+            plaintext = aes.decrypt(binascii.unhexlify(bytes(ciptriple[1], 'utf-8')))
+            return plaintext.decode()
 
         def decryptCTR(ciptriple):
             #we create a counter here since CTR requires a counter that never repeats so
             #no 2 blocks have the same IV. 
-            ctr = Counter.new(128, initial_value=long(ciptriple[2],16))
-            aes = AES.new(ciptriple[0].decode("hex"), AES.MODE_CTR, counter=ctr)
-            plaintext = aes.decrypt(ciptriple[1].decode("hex"))
-            return plaintext
+            ctr = Counter.new(128, initial_value=int.from_bytes(bytes(ciptriple[2], 'utf-8'), byteorder = 'big'))
+            aes = AES.new(binascii.unhexlify(bytes(ciptriple[0], 'utf-8')),\
+                          AES.MODE_CTR, counter=ctr)
+            plaintext = aes.decrypt(binascii.unhexlify(bytes(ciptriple[1],'utf-8')))
+            return plaintext.decode()
 
-        print "CBC messages:\n"
+        print("CBC messages:\n")
         i=1
         for ciptriple in CBCtextsandkeys:
-            print "Message %s: "% i, decryptCBC(ciptriple)
+            print("Message %s: "% i, decryptCBC(ciptriple))
             i += 1
-
-        print "\n","CTR messages:\n"
+        print()
+        print ("CTR messages:\n")
         i=1
 
         for ciptriple in CTRtextsandkeys:
-            print "Message %s: "% i, decryptCTR(ciptriple)
+            print("Message %s: "% i, decryptCTR(ciptriple))
             i += 1
 
         endefun()
 
     elif ende == "esc":
-        print "Quitting..."
+        print("Quitting...")
         
     else:
-        print "Invalid entry. Please enter 'e' or 'd'."
+        print("Invalid entry. Please enter 'e' or 'd'.")
         endefun()
 
 endefun()
